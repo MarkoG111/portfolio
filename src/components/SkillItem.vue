5676d1f02941f0964584008717fd0dea2c9a09f5
MarkoG111 <117818822+MarkoG111@users.noreply.github.com>
2025-11-19 11:03:48 +0100

    Add files via upload

---
+++ src/components/SkillItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex items-center gap-3">
+    <img :src="img" :alt="label" class="w-12 h-12 object-contain" />
+    <span class="font-bold text-gray-600">{{ label }}</span>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  img: String,
+  label: String
+});
+</script>
